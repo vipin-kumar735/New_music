fe96be8494ec6210fe215a4885a08d04ed821ce9
vipin-kumar735 <patelharimohan31@gmail.com>
2025-11-01 09:30:30 +0530

    Dilchor

--- BrandrdXMusic/plugins/admins/utag.py
+++ BrandrdXMusic/plugins/admins/utag.py
@@ -46,7 +46,7 @@ async def tag_all_users(_, message):
                 if usernum == 5:
                     await app.send_message(
                         message.chat.id,
-                        f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
+                        f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /stoputag ||",
                     )
                     usernum = 0
                     usertxt = ""
