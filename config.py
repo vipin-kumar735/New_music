ef6a951b6f2d44ca5483522d936f4739fdae53d8
vipin-kumar735 <patelharimohan31@gmail.com>
2025-11-01 08:40:39 +0530

    Dilchor

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_URL = getenv("API_URL", 'https://deadlinetech.site')
-API_KEY = getenv("API_KEY", 'mxZwH66586NuvJeofwrs')
+API_KEY = getenv("API_KEY", 'yHHBi26090dKZNrWhHZF')
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
