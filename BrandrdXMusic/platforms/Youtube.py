cd6fbb58953dfa496bf08d2006642b3de1edaeb8
vipin-kumar735 <patelharimohan31@gmail.com>
2025-12-08 00:44:13 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -23,31 +23,6 @@ import traceback
 from BrandrdXMusic import LOGGER
 
 API_URL = "https://teaminflex.xyz"  # Change to your API server URL
-API_KEY = "import asyncio
-import os
-import re
-import json
-from typing import Union
-import requests
-import yt_dlp
-from pyrogram.enums import MessageEntityType
-from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
-from BrandrdXMusic.utils.database import is_on_off
-from BrandrdXMusic import app
-from BrandrdXMusic.utils.formatters import time_to_seconds
-import os
-import glob
-import random
-import logging
-import pymongo
-from pymongo import MongoClient
-import aiohttp
-import config
-import traceback
-from BrandrdXMusic import LOGGER
-
-API_URL = "https://teaminflex.xyz"  # Change to your API server URL
 API_KEY = "INFLEX86599428D"
 
 def cookie_txt_file():
