93d14429e7dd5757a992636ba4ffeada5b075521
vipin-kumar735 <patelharimohan31@gmail.com>
2025-12-14 18:06:56 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -1,15 +1,26 @@
 import asyncio
 import os
 import re
+import json
 from typing import Union
+import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
+from BrandrdXMusic.utils.database import is_on_off
+from BrandrdXMusic import app
 from BrandrdXMusic.utils.formatters import time_to_seconds
+import os
+import glob
+import random
+import logging
+import pymongo
+from pymongo import MongoClient
 import aiohttp
+import config
+import traceback
 from BrandrdXMusic import LOGGER
-from typing import Union
 
 YOUR_API_URL = None
 FALLBACK_API_URL = "https://shrutibots.site"
