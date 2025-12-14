8723a26cce4c13be9101e4e8f40e30db885552a3
vipin-kumar735 <patelharimohan31@gmail.com>
2025-12-14 09:04:50 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- config.py
+++ config.py
@@ -33,9 +33,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", None) 
+API_URL = getenv("API_URL", 'https://console.nexgenbots.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.console.nexgenbots.xyz')
+API_KEY = getenv("API_KEY", 30DxNexGenBots3d34cb') 
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
